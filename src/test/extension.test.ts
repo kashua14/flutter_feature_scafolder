5672b1396b8d3192cfa05b75a9d66d4ca8497615
Kasasira Joshua <31764137+kashua14@users.noreply.github.com>
2024-04-01 14:02:08 +0300

    🎯 Added a test case

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,10 @@
-import * as assert from 'assert';
+const assert = require('assert');
+const vscode = require('vscode');
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
-
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
-
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+suite("Extension Tests", function() {
+    test("Sample test", async () => {
+        const command = await vscode.commands.getCommands(true);
+        const commandExists = command.includes('flutter_feature_scaffolder.scaffoldFeature');
+        assert.equal(commandExists, true);
+    });
 });
